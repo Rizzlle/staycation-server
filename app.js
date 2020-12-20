7fe3179d5b450dc9800277ca2c904313125bca9f
Rizzlle <hariyan242@gmail.com>
2020-12-20 16:42:19 +0800

    adding cors

--- app.js
+++ app.js
@@ -56,6 +56,20 @@ app.use(
     )
 );
 
+// for cors
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
